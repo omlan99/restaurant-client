50c2a5fcc32abefb5219807b37538d1b36d4c00a
omlan99 <omlan10616@gmail.com>
2025-01-13 23:03:57 +0600

    prject setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+      <button className='btn'>plain 1</button>
+      <button className='btn btn-primary btn-wide'>plain 2</button>
+     
+    </>
+  )
+}
+
+export default App
