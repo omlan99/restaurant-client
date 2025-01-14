c359de4ca38bcfe4e946ff6e928a32488fb3dc8c
omlan99 <omlan10616@gmail.com>
2025-01-15 01:47:29 +0600

    added react tab

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -1,11 +1,32 @@
-import Cover from '../../Component/Cover';
-import shopImg from "../../assets/shop/banner2.jpg"
+import Cover from "../../Component/Cover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import shopImg from "../../assets/shop/banner2.jpg";
+import 'react-tabs/style/react-tabs.css';
+import { useState } from "react";
 const Order = () => {
-    return (
-        <div>
-            <Cover image={shopImg} title={"Order Food"} ></Cover>
-        </div>
-    );
+    const [tabIndex, setTabIndex] = useState()
+  return (
+    <div>
+      <Cover image={shopImg} title={"Order Food"}></Cover>
+      <div className="flex justify-center uppercase">
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList>
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soups</Tab>
+          <Tab>Desserts</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+        <TabPanel></TabPanel>
+      </Tabs>
+      </div>
+ 
+    </div>
+  );
 };
 
 export default Order;
