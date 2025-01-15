5ba62121426ee76c890bf40923a6d10280ea75ed
omlan99 <omlan10616@gmail.com>
2025-01-15 21:14:41 +0600

    added context

---
+++ src/Context/AuthProvider.jsx
@@ -0,0 +1,67 @@
+import { createUserWithEmailAndPassword, getRedirectResult, GoogleAuthProvider, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signInWithRedirect, signOut, updateProfile } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { createContext } from "react";
+import auth from '../../Firebase/Firebase.init';
+
+export const AuthContext = createContext(null)
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState (null)
+    const [loader, setLoader] = useState(true)
+
+    const createUser = (email, password) => {
+        setLoader(true)
+        return createUserWithEmailAndPassword(auth, email, password);
+      };
+
+    const signInUser = (email, password) => {
+        setLoader(true)
+        return signInWithEmailAndPassword(auth, email, password);
+    };  
+    const googleSignIn = () => {
+        setLoader(true)
+        return signInWithPopup(auth, googleProvider);
+      };
+
+      useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
+          console.log("User logged in ", loggedUser);
+          setUser(loggedUser);
+          setLoading(false)
+     
+        });
+        return () => {
+          unsubscribe();
+        };
+      }, []);
+      const updateUser= (updateData) =>{
+        return updateProfile(auth.currentUser, updateData)
+      }
+      const signOutUser = () => {
+        setLoader(true)
+
+        return signOut(auth);
+      };
+      const resetPassword = (email) =>{
+        return sendPasswordResetEmail(auth, email)
+      }
+      
+    const authValue = {
+        user,
+        setUser,
+        loader,
+        createUser,
+        signInUser,
+        googleSignIn,
+        updateUser,
+        signOutUser, 
+        resetPassword
+
+    }
+    return (
+        <AuthContext.Provider value={authValue}>{children}</AuthContext.Provider>
+     );
+};
+
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router/Routes.jsx";
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./Context/AuthProvider.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
+  <AuthProvider>
+  <HelmetProvider>
       <div className="max-w-screen-xl mx-auto">
         <RouterProvider router={router} />
       </div>
     </HelmetProvider>
+  </AuthProvider>
   </StrictMode>
 );
