4964666e89c6e4175fd0a97adfbba4001d0cd402
omlan99 <omlan10616@gmail.com>
2025-01-18 21:30:50 +0600

    added make admin functionality

--- src/Pages/Dashboard/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers.jsx
@@ -2,17 +2,79 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import useAxiosSecure from "../../Hook/useAxiosSecure";
 import { FaTrash, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: users = [] } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
       return res.data;
     },
   });
-  const handleDelete = () => {};
+  const handleDelete = (id) => {
+      const swalWithBootstrapButtons = Swal.mixin({
+          customClass: {
+            confirmButton: "btn btn-success",
+            cancelButton: "btn btn-danger",
+          },
+          buttonsStyling: false,
+        });
+        swalWithBootstrapButtons
+          .fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel!",
+            reverseButtons: true,
+          })
+          .then((result) => {
+            if (result.isConfirmed) {
+           
+              axiosSecure.delete(`/users/${id}`)
+              .then(res =>{
+                console.log(res.data)
+                if(res.data.deletedCount > 0){
+                             swalWithBootstrapButtons.fire({
+                  title: "Deleted!",
+                  text: "Your file has been deleted.",
+                  icon: "success"
+                });
+                refetch()
+                }
+              })
+            } else if (
+              /* Read more about handling dismissals below */
+              result.dismiss === Swal.DismissReason.cancel
+            ) {
+              swalWithBootstrapButtons.fire({
+                title: "Cancelled",
+                text: "Your imaginary file is safe :)",
+                icon: "error",
+              });
+            }
+          });
+  };
+  const handleMakeAdmin = (user) => {
+    axiosSecure.patch(`/users/admin/${user._id}`)
+    .then(res =>{
+      console.log(res.data)
+      if(res.data.modifiedCount > 0){
+        refetch()
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `${user.name} is an admin now`,
+            showConfirmButton: false,
+            timer: 1500
+          });
+      }
+    })
+    console.log(user)
+  }
   return (
     <div className="w-full px-5">
       <div className="flex justify-between ">
@@ -38,12 +100,14 @@ const AllUsers = () => {
                 <td>{user.name} </td>
                 <td>{user.email}</td>
                 <td>
-                  <button
-                    
+                 {
+                    user.role === 'admin'   ? "Admin" :  <button
+                    onClick={() => handleMakeAdmin(user)}
                     className="btn  btn-lg bg-orange-400 text-white text-2xl"
                   >
                     <FaUsers  />
                   </button>
+                 }
                 </td>
                 <td>
                   {" "}
