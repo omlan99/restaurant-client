bb8431ed20c0643aafa94d0c94eceb6843ceb2ad
omlan99 <omlan10616@gmail.com>
2025-01-19 18:53:39 +0600

    added menu to server

--- src/Pages/Dashboard/AddItem.jsx
+++ src/Pages/Dashboard/AddItem.jsx
@@ -3,21 +3,45 @@ import SectionTitle from "../../Component/SectionTitle";
 import { useForm } from "react-hook-form";
 import { FaUtensils } from "react-icons/fa";
 import useAxiosPublic from "../../Hook/useAxiosPublic";
+import useAxiosSecure from "../../Hook/useAxiosSecure";
+import Swal from "sweetalert2";
 const image_hosting_key = import.meta.env.VITE_image_hosting_key;
-const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 const AddItem = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
   const onSubmit = async (data) => {
-    console.log(data)
-    const imageFile = {image: data.image[0]}
-   const res = await axiosPublic.post(image_hosting_api, imageFile, {
-    headers : {
-        'Content-Type' : 'multipart/form-data'
+    console.log(data);
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    if (res.data.success) {
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: parseFloat(data.price),
+        recipe: data.recipe,
+        image: res.data.data.display_url,
+      };
+      const menuRes = await axiosSecure.post("/menu", menuItem);
+      console.log(menuRes.data);
+      if (menuRes.data.insertedId) {
+        reset()
+        Swal.fire({
+          position: "top",
+          icon: "success",
+          title: `${data.name} added to the menu`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
     }
-   })
-   console.log(res.data)
-};
+    console.log(res.data);
+  };
 
   return (
     <div>
@@ -45,11 +69,11 @@ const AddItem = () => {
                 <span className="label-text">Select a Category</span>
               </div>
               <select
-                defaultValue={'value'}
+                defaultValue={"value"}
                 className="select select-bordered w-full "
                 {...register("category")}
               >
-                <option disabled value='value'>
+                <option disabled value="value">
                   Select a Category
                 </option>
                 <option value="salad">Salad</option>
@@ -78,15 +102,21 @@ const AddItem = () => {
               <span className="label-text">Recipe Details</span>
             </div>
             <textarea
-             {...register('recipe')}
+              {...register("recipe")}
               className="textarea textarea-bordered h-24"
               placeholder="Recipe Details"
             ></textarea>
           </label>
-        <div className="form-control w-full my-6">
-        <input {...register('image')} type="file" className="file-input w-full max-w-xs" />
-        </div>
-         <button className="btn ">Add Item <FaUtensils className="ml-4"></FaUtensils></button>
+          <div className="form-control w-full my-6">
+            <input
+              {...register("image")}
+              type="file"
+              className="file-input w-full max-w-xs"
+            />
+          </div>
+          <button className="btn ">
+            Add Item <FaUtensils className="ml-4"></FaUtensils>
+          </button>
         </form>
       </div>
     </div>
